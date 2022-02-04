e557e3523759a9eed7884e76a69a6927a2bf782e
mkasarci <muhammedkasarci@gmail.com>
2022-02-04 21:32:54 +0100

    Added Episode, TvShow, User and UserTvShow entities.

--- MyShow.Data/Entities/TvShow.cs
+++ MyShow.Data/Entities/TvShow.cs
@@ -2,4 +2,8 @@
 
 public class TvShow
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime NextEpisode { get; set; }
+    public string ImageUrl { get; set; }
 }
---
+++ MyShow.Data/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyShow.Data.Entities;
+
+public class User : IdentityUser
+{
+
+}
