79eaa8af6383f0b56d8c1cd70f087ce0dcb44031
mkasarci <muhammedkasarci@gmail.com>
2022-02-05 22:00:46 +0100

    * Added necessary configurations to the mappings.

--- MyShow.Data/Services/Maze/MazeProfile.cs
+++ MyShow.Data/Services/Maze/MazeProfile.cs
@@ -7,7 +7,10 @@ public class MazeProfile : Profile
 {
     public MazeProfile()
     {
-        CreateMap<MazeEpisode, Episode>();
-        CreateMap<MazeTvShow, TvShow>();
+        CreateMap<MazeEpisode, Episode>()
+            .ForMember(destination => destination.Id, option => option.Ignore());
+        CreateMap<MazeTvShow, TvShow>()
+            .ForMember(destination => destination.Id, option => option.Ignore())
+            .ForMember(destination => destination.NextEpisode, option => option.MapFrom(source => source.Embedded.NextEpisode));
     }
 }
