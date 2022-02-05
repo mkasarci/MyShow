993f488d2982d932555fe5300362a057d6ced8c4
chrisK00 <christian.khalil2000@gmail.com>
2022-02-05 20:45:10 +0100

    Changed UserId FK from int to string (GUID) which is IdentityUser's default

--- MyShow.Data/AppDbContext.cs
+++ MyShow.Data/AppDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace MyShow.Data;
 
-public class AppDbContext : IdentityDbContext
+public class AppDbContext : IdentityDbContext<User>
 {
+    public DbSet<TvShow> TvShows { get; set; }
+    public DbSet<UserTvShow> UserTvShows { get; set; }
+
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
@@ -12,5 +16,11 @@ public class AppDbContext : IdentityDbContext
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.Entity<UserTvShow>()
+            .HasKey(x => new { x.UserId, x.TvShowId });
+
+        builder.Entity<Episode>()
+            .ToTable("Episodes");
     }
 }
--- MyShow.Data/Migrations/20220205194316_InitEntities.Designer.cs
+++ MyShow.Data/Migrations/20220205194316_InitEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyShow.Data;
 
@@ -11,9 +12,10 @@ using MyShow.Data;
 namespace MyShow.MVC.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220205194316_InitEntities")]
+    partial class InitEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
